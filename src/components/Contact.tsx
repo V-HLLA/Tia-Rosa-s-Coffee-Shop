0a61ebb2b1974f0a3c44e0bc8e2ecbe490323352
V <downfiresword@gmail.com>
2025-04-06 23:24:17 -0300

    Fixed the height of the cards in the products page and added a hover effect to navButtons.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,48 @@
 export default function Contact() {
   return (
     <>
-      <main>contatos</main>
+      <main className="main-container fade-in">
+        <h1 className="section-title">Fale com a gente</h1>
+        <div className="content-container">
+          <div className="card-container fade-in">
+            <p className="description-title">
+              Estamos prontos para ouvir você!
+            </p>
+            <p className="description-text">
+              Tem alguma dúvida, sugestão ou deseja saber mais sobre nossos
+              produtos e serviços? Preencha o formulário abaixo ou entre em
+              contato pelos nossos canais oficiais. Será um prazer atender você!
+            </p>
+          </div>
+
+          <div className="card-container fade-in">
+            <p className="description-title">Informações de Contato:</p>
+            <p className="description-text">
+              Endereço: Av. das Cafeteiras, nº 123 - Brasília, DF Telefone: (61)
+              9 9999-9999 E-mail: contato@tiarosacoffee.com.br
+            </p>
+          </div>
+
+          <div className="card-container fade-in">
+            <p className="description-title">Redes Sociais:</p>
+            <p className="description-text">
+              Siga e acompanhe nossas novidades: Instagram: @tiarosacoffee
+              Facebook: Coffee Shops Tia Rosa TikTok: @tiarosacoffee
+            </p>
+          </div>
+
+          <div className="card-container fade-in">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15348.32149429825!2d-47.806836399999995!3d-15.904859649999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a21d4f88603d5%3A0x5751b3bbc52f0802!2sPapuda%20-%20S%C3%A3o%20Sebasti%C3%A3o%2C%20Bras%C3%ADlia%20-%20DF!5e0!3m2!1spt-BR!2sbr!4v1743873735916!5m2!1spt-BR!2sbr"
+              width="300"
+              height="300"
+              allowFullScreen={false}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,9 @@ export default function Footer() {
   return (
     <footer className="footer">
       <ul className="social_media">
-        <li>Instagram</li>
-        <li>Facebook</li>
+        <li>Instagram: @tiarosacoffee</li>
+        <li>Facebook: Coffee Shops Tia Rosa</li>
+        <li>TikTok: @tiarosacoffee</li>
       </ul>
       <address>
         Endereço: Setor Comercial Sul Q. 6 Quadra 8 <br /> Asa Sul, Brasília -
